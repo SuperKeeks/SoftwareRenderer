afb4f4082f20981db634c06b50deadfe9f7fd481
Enrique Alcor <enrique.alcor@mediatonic.co.uk>
2015-12-30 20:51:02 +0000

    LogUtils for logging in a multiplatform way

---
+++ SoftwareRenderer/LogUtils.cpp
@@ -0,0 +1,37 @@
+//
+//  LogUtils.cpp
+//  SoftwareRenderer
+//
+//  Created by Enrique Alcor Martín on 30/12/15.
+//  Copyright (c) 2015 Enrique Alcor Martín. All rights reserved.
+//
+
+#include "LogUtils.h"
+
+#include <cstdarg>
+#include <cstdio>
+#include <string>
+
+#ifdef _WINDOWS
+#include <Windows.h>
+#endif
+
+namespace omb
+{
+
+void Log::Debug(const char* fmt, ...)
+{
+	va_list args;
+	va_start(args, fmt);
+	const int bufferSize = 1024;
+	char buff[bufferSize];
+	vsnprintf(buff, bufferSize, fmt, args);
+#ifdef _WINDOWS
+	OutputDebugString(buff);
+#elif defined(__APPLE__)
+	printf(buff);
+#endif
+	va_end(args);
+}
+
+}
---
+++ SoftwareRenderer/LogUtils.h
@@ -0,0 +1,19 @@
+//
+//  LogUtils.h
+//  SoftwareRenderer
+//
+//  Created by Enrique Alcor Martín on 30/12/15.
+//  Copyright (c) 2015 Enrique Alcor Martín. All rights reserved.
+//
+
+#pragma once
+
+namespace omb
+{
+
+namespace Log
+{
+	void Debug(const char* fmt, ...);
+}
+
+}
